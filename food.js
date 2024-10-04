24d73e4e3811323f7bff56cc9fbc3e6adf4e23e7
Grivie <grivieindonesia@gmail.com>
2024-10-04 13:14:35 +0700

    Update food.js

--- food.js
+++ food.js
@@ -1,7 +1,24 @@
-document.getElementById("searchForm").addEventListener("submit",function(event){event.preventDefault();const input=document.getElementById("searchInput").value;const formattedInput=input.split(" ").join("_");const url=`action://act/search/${formattedInput}`;window.location.href=url;});const nama="{nama}";const makanan=["Nasi Goreng","Mie Ayam","Sate Ayam","Bakso","Rendang","Gado-Gado","Soto Ayam","Martabak","Ayam Goreng","Pizza"];function updatePlaceholder(){const randomMakanan=makanan[Math.floor(Math.random()*makanan.length)];const placeholderText=`${randomMakanan}?`;document.getElementById("searchInput").placeholder=placeholderText;}
-function openPopup(){document.getElementById("popupcabang").style.display="block";}
-function closePopup(){document.getElementById("popupcabang").style.display="none";}
-function openPopup2(){document.getElementById("popupcabang2").style.display="block";}
-function closePopup2(){document.getElementById("popupcabang2").style.display="none";}
-document.getElementById("profile-item").onclick=function(){const profileFullscreen=document.getElementById("profile-fullscreen");profileFullscreen.style.display=profileFullscreen.style.display==="none"||profileFullscreen.style.display===""?"block":"none";};function closeProfile(){document.getElementById("profile-fullscreen").style.display="none";}
-function showPromoBox(){const promoBox=document.getElementById("promoBox");promoBox.style.display=promoBox.style.display==="none"||promoBox.style.display===""?"block":"none";}
+
+function openPopup() {
+    document.getElementById("popupcabang").style.display = "block";
+}
+function closePopup() {
+    document.getElementById("popupcabang").style.display = "none";
+}
+function openPopup2() {
+    document.getElementById("popupcabang2").style.display = "block";
+}
+function closePopup2() {
+    document.getElementById("popupcabang2").style.display = "none";
+}
+document.getElementById("profile-item").onclick = function () {
+    const profileFullscreen = document.getElementById("profile-fullscreen");
+    profileFullscreen.style.display = profileFullscreen.style.display === "none" || profileFullscreen.style.display === "" ? "block" : "none";
+};
+function closeProfile() {
+    document.getElementById("profile-fullscreen").style.display = "none";
+}
+function showPromoBox() {
+    const promoBox = document.getElementById("promoBox");
+    promoBox.style.display = promoBox.style.display === "none" || promoBox.style.display === "" ? "block" : "none";
+}
