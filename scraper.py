a80fd5ec9a4c224b3496053d99a9405975d8c464
Grivie <grivieindonesia@gmail.com>
2025-07-02 22:48:31 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,10 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import undetected_chromedriver as uc
 
-TARGET_URL = "https://www.jagel.id/app/grivie-447992/g-food-3131950"
-OUTPUT_FILENAME = "product.json"
-PAGES_FOR_UPDATE = 5
-PAGES_FOR_FULL_SCRAPE = 999
+CONFIG
 
 def setup_driver():
     print("✨ Initializing virtual browser...")
